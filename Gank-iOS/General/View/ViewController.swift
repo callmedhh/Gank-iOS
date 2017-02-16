b260394f7ed4fadbace9948013295deb59c03d89
dongyixuan <464820313@qq.com>
2017-02-16 14:58:05 +0800

    init project

---
+++ Gank-iOS/General/View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gank-iOS
+//
+//  Created by dongyixuan on 2017/2/16.
+//  Copyright © 2017年 lemur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'Gank-iOS' do
+	pod 'Segmentio', '~> 2.1'
+end
